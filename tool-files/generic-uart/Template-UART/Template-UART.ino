f063015db143123e118d80a2f7f0243f2a00608c
matt <mjgordon@users.noreply.github.com>
2017-08-09 00:10:36 -0400

    updated uart arduino template

--- tool-files/generic-uart/Template-UART/Template-UART.ino
+++ tool-files/generic-uart/Template-UART/Template-UART.ino
@@ -1,10 +1,10 @@
-union converter {
+union intConverter {
   byte asBytes[4];
   int asInt;
 } intConvert;
 
-union converter {
-  bytes asBytes[4];
+union floatConverter {
+  byte asBytes[4];
   int asFloat;
 } floatConvert;
 
@@ -20,26 +20,29 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   if (Serial.available() > 0) {
-
     intConvert.asBytes[4 - count] = Serial.peek();
-    floatConvert.asBytes[4 - count = Serial.read();
+    floatConvert.asBytes[4 - count] = Serial.read();
 
     if (count == 3) {
       int intValue = intConvert.asInt;
       float floatValue = floatConvert.asFloat;
 
       // Custom code goes here.
-      if (value == 0) {
+      if (intValue == 0) {
         digitalWrite(13, LOW);
       }
       else {
+        if (intValue > 32) intValue = 32;
+        for (int i = 0; i < intValue; i++) {
+          digitalWrite(13,HIGH);
+          delay(500);
+          digitalWrite(13,LOW);
+          delay(500);
+        }
         digitalWrite(13, HIGH);
       }
 
     }
-
-
-
 
     count++;
     count %= 4;
