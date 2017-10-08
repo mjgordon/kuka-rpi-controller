5e73680554f4eb229099692dfd24dead0d22a775
mjgordon <->
2017-10-08 18:54:50 -0400

    updated gh template

--- tool-files/generic-uart/Template-UART/Template-UART.ino
+++ tool-files/generic-uart/Template-UART/Template-UART.ino
@@ -1,9 +1,9 @@
 /*
   This template is a base for tools that use an arduino and will communicate over uart-serial
   (i.e. with a single microcontroller attached. See i2c for talking to multiple arduinos or other boards).
-  No changes need to be made for connecting over USB vs hardware pins (remember to use a proper 
+  No changes need to be made for connecting over USB vs hardware pins (remember to use a proper
   logic converter for connecting over pins).
-  Any code that uses data received from the serial connection should be added to the relevant section 
+  Any code that uses data received from the serial connection should be added to the relevant section
   in the checkSerial function.
 */
 
@@ -17,7 +17,10 @@ union floatConverter {
   float asFloat;
 } floatConvert;
 
-int count = 0;
+int byteCount = 0;
+int intValue[4];
+float floatValue[4];
+
 
 
 void setup() {
@@ -27,38 +30,43 @@ void setup() {
 
 
 void loop() {
+  //This must always be called first
   checkSerial();
+
+  // === ADD CUSTOM TOOL CODE HERE ===
+  if (intValue == 0) {
+    digitalWrite(13, LOW);
+  }
+  else {
+    if (intValue[0] > 32) intValue[1] = 32;
+    for (int i = 0; i < intValue[1]; i++) {
+      digitalWrite(13, HIGH);
+      delay(500);
+      digitalWrite(13, LOW);
+      delay(500);
+    }
+  }
+
+  // === END CUSTOM CODE ===
 }
 
 /*
-Reads 4-byte integers or floats. The value sent from the robot will be available 
-in the intValue or floatValue variables.
+  Reads four 4-byte integers or floats. The value sent from the robot will be available
+  in the intValue or floatValue variables.
 */
 void checkSerial() {
   while (Serial.available() > 0) {
-    intConvert.asBytes[4 - count] = Serial.peek();
-    floatConvert.asBytes[4 - count] = Serial.read();
+    int varId = byteCount / 4;  // Which variable
+    int innerId = byteCount % 4; // Which byte within variable
+    
+    intConvert.asBytes[4 - innerId] = Serial.peek();
+    floatConvert.asBytes[4 - innerId] = Serial.read();
 
-    if (count == 3) {
-      int intValue = intConvert.asInt;
-      float floatValue = floatConvert.asFloat;
-
-      // === REPLACE WITH CUSTOM CODE THAT USES SERIAL DATA ===
-      if (intValue == 0) {
-        digitalWrite(13, LOW);
-      }
-      else {
-        if (intValue > 32) intValue = 32;
-        for (int i = 0; i < intValue; i++) {
-          digitalWrite(13,HIGH);
-          delay(500);
-          digitalWrite(13,LOW);
-          delay(500);
-        }
-      }
-      // === END CUSTOM CODE ===
+    if (byteCount % 4 == 3) {
+      intValue[varId] = intConvert.asInt;
+      floatValue[varId] = floatConvert.asFloat;
     }
-    count++;
-    count %= 4;
+    byteCount++;
+    byteCount %= 16;
   }
 }
