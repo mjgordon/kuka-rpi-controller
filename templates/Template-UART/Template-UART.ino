199ade3b7111399b6029869af2870e2b2560d6d8
mjgordon <->
2019-08-05 19:55:15 -0400

    Updated Arduino UART Template

--- templates/Template-UART/Template-UART.ino
+++ templates/Template-UART/Template-UART.ino
@@ -1,72 +1,92 @@
 /*
   This template is a base for tools that use an arduino and will communicate over uart-serial
   (i.e. with a single microcontroller attached. See i2c for talking to multiple arduinos or other boards).
-  No changes need to be made for connecting over USB vs hardware pins (remember to use a proper
-  logic converter for connecting over pins).
-  Any code that uses data received from the serial connection should be added to the relevant section
-  in the checkSerial function.
+  No changes should be necessary for connecting over USB vs hardware pins (remember to use a proper
+  logic converter for connecting via pins).
+  The main loop contains a call to checkSerial, and a switch statement against the received command. New commands
+  should be defined within this switch statement. 
+  This template is agnostic as to whether the data being sent is an int or a float. Both are available
+  from the received bytes in the serialDataInt and serialDataFloat variables
 */
 
-union intConverter {
+/* 
+ *  This union facilitates conversion from a series of bytes to the 4-byte ints or floats they represent
+ */
+union byteConverter {
   byte asBytes[4];
   int asInt;
-} intConvert;
-
-union floatConverter {
-  byte asBytes[4];
   float asFloat;
-} floatConvert;
+} converter;
 
-int byteCount = 0;
-int intValue[4];
-float floatValue[4];
+/*
+ * Contains the 1-byte command received from the serial message
+ */
+byte serialCommand = 0;
+
+/*
+ * Contains the 4-byte integer or float representation of the data from the serial message
+ */
+int serialDataInt = 0;
+float serialDataFloat = 0;
+
+/*
+ * Keeps track of serial data as its received
+ */
+byte serialCount = 0;
+
+
+/*
+ * Example command. Commands are best described with #define for easy readability
+ */
+#define COMMAND_ON_OFF 0
 
 
 
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); // Used with example command
   Serial.begin(9600);
 }
 
 
 void loop() {
-  //This must always be called first
-  checkSerial();
+  if (checkSerial()) {
+    switch (serialCommand) {
+      
+      // Example command
+      case COMMAND_ON_OFF:
+        digitalWrite(13, serialDataInt);
+        break;
 
-  // === ADD CUSTOM TOOL CODE HERE ===
-  if (intValue == 0) {
-    digitalWrite(13, LOW);
-  }
-  else {
-    if (intValue[0] > 32) intValue[1] = 32;
-    for (int i = 0; i < intValue[1]; i++) {
-      digitalWrite(13, HIGH);
-      delay(500);
-      digitalWrite(13, LOW);
-      delay(500);
+        
+        // === ADD CUSTOM TOOL COMMANDS HERE ===
+
+        // === END CUSTOM CODE ===
     }
   }
-
-  // === END CUSTOM CODE ===
 }
 
 /*
-  Reads four 4-byte integers or floats. The value sent from the robot will be available
-  in the intValue or floatValue variables.
-*/
-void checkSerial() {
+ * Receives bytes from the serial connection, and stores them in the converter union. 
+ * Returns true if a full message of 5 bytes has just finished being received
+ */
+boolean checkSerial() {
   while (Serial.available() > 0) {
-    int varId = byteCount / 4;  // Which variable
-    int innerId = byteCount % 4; // Which byte within variable
-    
-    intConvert.asBytes[4 - innerId] = Serial.peek();
-    floatConvert.asBytes[4 - innerId] = Serial.read();
-
-    if (byteCount % 4 == 3) {
-      intValue[varId] = intConvert.asInt;
-      floatValue[varId] = floatConvert.asFloat;
+    if (serialCount == 0) {
+      serialCommand = Serial.read();
     }
-    byteCount++;
-    byteCount %= 16;
+    else {
+      converter.asBytes[serialCount - 1] = Serial.read();
+    }
+
+    if (serialCount == 4) {
+      serialCount = 0;
+      serialDataInt = converter.asInt;
+      serialDataFloat = converter.asFloat;
+      return true;
+    }
+    else {
+      serialCount += 1;
+    }
   }
+  return false;
 }
